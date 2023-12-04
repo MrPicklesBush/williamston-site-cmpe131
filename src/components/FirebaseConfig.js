c96673b96466706f9cc4db55fe55e7b4f7d64204
MrPicklesBush <bush.nguyen02@gmail.com>
2023-12-03 18:03:11 -0800

    fix some of the login/register and i add sign out button when logged in

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -14,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const database = getAuth(app)
+
--- src/components/Register_LoginSection.js
+++ src/components/Register_LoginSection.js
@@ -1,69 +1,128 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import '../App.css';
+import { Button } from './Button';
 import { database } from './FirebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { sendPasswordResetEmail } from 'firebase/auth';
 import './Register_LoginSection.css';
 
 function Register_LoginSection() {
-  const [login,setLogin] = useState(false)
+  const [login, setLogin] = useState(false);
+  const [buttonText, setButtonText] = useState('Sign In');
+  const [buttonLink, setButtonLink] = useState('/register-login');
+  const [user, setUser] = useState(null);
+  const history = useNavigate();
 
-  const history = useNavigate()
+  useEffect(() => {
+    // Listener to check if the user is signed in
+    const authStateChanged = () => {
+      const auth = database.currentUser;
+      if (auth) {
+        setUser(auth);
+      } else {
+        setUser(null);
+      }
+    };
 
-  const handleSubmit = (e,type) => {
+    // Set up the listener
+    const unsubscribe = database.onAuthStateChanged(authStateChanged);
+
+    // Clean up the listener on component unmount
+    return () => unsubscribe();
+  }, []);
+
+  const handleSubmit = (e, type) => {
     e.preventDefault();
-    console.log(e.target.email.value);
-    const email = e.target.email.value
-    const password = e.target.password.value
-    if(type == 'signup'){
-      createUserWithEmailAndPassword(database,email,password).then(data=>{
-        console.log(data,"authData")
-        history('/')
-      }).catch(err=>{
-        alert(err.code)
-        setLogin(true)
-      })
-    }else{
-      signInWithEmailAndPassword(database,email,password).then(data=>{
-        console.log(data,"authData")
-        history('/')
-      }).catch(err=>{
-        alert(err.code)
-      })
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    if (type === 'signup') {
+      createUserWithEmailAndPassword(database, email, password)
+        .then((data) => {
+          console.log(data, 'authData');
+          history('/');
+        })
+        .catch((err) => {
+          alert(err.code);
+          setLogin(true);
+        });
+    } else {
+      signInWithEmailAndPassword(database, email, password)
+        .then((data) => {
+          console.log(data, 'authData');
+          history('/');
+        })
+        .catch((err) => {
+          alert(err.code);
+        });
     }
   };
 
-  const handleReset = ()=>{
-    history("/reset");
-  }
+  const handleReset = () => {
+    const email = prompt('Enter your email:');
+    if (email) {
+      sendPasswordResetEmail(database, email)
+        .then(() => {
+          alert('Password reset email sent. Check your inbox!');
+        })
+        .catch((err) => {
+          alert(err.code);
+        });
+    }
+  };
+
+  const handleSignOut = () => {
+    signOut(database)
+      .then(() => {
+        history('/');
+      })
+      .catch((err) => {
+        alert(err.code);
+      });
+  };
+
+  const toggleButton = () => {
+    setLogin(!login);
+    setButtonText(login ? 'Sign In' : 'Sign Up');
+    setButtonLink(login ? '/register-login' : '/register');
+  };
 
   return (
     <div className="register-container">
-      {/* Register and login Screen */}
       <div className="row">
-        <div
-          className={login == false ? "activeColor" : "pointer"}
-          onClick={() => setLogin(false)}
-        >
-          SignUp
-        </div>
-        <div
-          className={login == true ? "activeColor" : "pointer"}
-          onClick={() => setLogin(true)}
-        >
-          SignIn
-        </div>
+        {user ? (
+          <button className="sign-out-btn" onClick={handleSignOut}>
+            Sign Out
+          </button>
+        ) : (
+          <>
+            <div className={login ? 'pointer' : 'activeColor'} onClick={toggleButton}>
+              Sign Up
+            </div>
+            <div className={login ? 'activeColor' : 'pointer'} onClick={toggleButton}>
+              Sign In
+            </div>
+          </>
+        )}
       </div>
-      <h1>{login ? "SignIn" : "SignUp"}</h1>
-      <form onSubmit={(e) => handleSubmit(e, login ? "signin" : "signup")}>
+      <h1>{login ? 'Sign In' : 'Sign Up'}</h1>
+      <form onSubmit={(e) => handleSubmit(e, login ? 'signin' : 'signup')}>
         <input name="email" placeholder="Email" />
         <br />
-        <input name="password" type="text" placeholder="Password" />
+        <input name="password" type="password" placeholder="Password" />
         <br />
-        <p onClick={handleReset}>Forgot Password?</p>
+        {login && (
+          <p onClick={handleReset} className="forgot-password">
+            Forgot Password?
+          </p>
+        )}
         <br />
-        <button>{login ? "SignIn" : "SignUp"}</button>
+        <button>{login ? 'Sign In' : 'Sign Up'}</button>
       </form>
+      <Link to={buttonLink}>
+        <button className="toggle-btn">{buttonText}</button>
+      </Link>
     </div>
   );
 }
